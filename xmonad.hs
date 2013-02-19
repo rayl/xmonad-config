99217272238a0985eec50cc7140df204d8a7f43d
Ray Lehtiniemi <rayl@mail.com>
2013-02-19 13:20:47 -0700

    Import my current bindings
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,10 @@
+import qualified Data.Map as M
+import System.Exit
+import System.IO
 import XMonad
+import XMonad.Actions.CycleWS (nextWS, prevWS, toggleWS, toggleOrView)
+import XMonad.Actions.SwapWorkspaces
+import XMonad.Actions.WindowBringer
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.MouseResizableTile
@@ -7,9 +13,9 @@ import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.Named (named)
 import XMonad.Layout.NoBorders
+import qualified XMonad.StackSet as W
 import XMonad.Util.Cursor
 import XMonad.Util.Run(spawnPipe)
-import System.IO
 
 home = "/home/local/.xmonad/"
 
@@ -18,6 +24,9 @@ main = do
     xmonad $ defaultConfig
         { borderWidth        = 2
         , workspaces         = myWorkspaces
+        , modMask            = myModMask
+        , keys               = myKeys
+        , mouseBindings      = myMouseBindings
         , startupHook        = setDefaultCursor xC_left_ptr
         , layoutHook         = myLayoutHook
         , manageHook         = manageHook defaultConfig <+> manageDocks
@@ -58,3 +67,68 @@ myTransforms = smartBorders
              . avoidStruts
              . mkToggle (single NBFULL)
 
+------------------------------------------------------------------------
+-- BINDINGS
+------------------------------------------------------------------------
+modm = mod4Mask
+modS = modm .|. shiftMask
+modC = modm .|. controlMask
+
+myModMask = modm
+
+-- Key bindings
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+	[ ((modm, xK_q       ), restart "xmonad" True)
+        , ((modS, xK_q       ), io (exitWith ExitSuccess))
+        , ((modm, xK_t       ), withFocused $ windows . W.sink)
+        , ((modm, xK_u       ), prevWS)
+        , ((modm, xK_i       ), toggleWS)
+        , ((modm, xK_o       ), nextWS)
+
+        , ((modm, xK_a       ), spawn $ XMonad.terminal conf)
+        , ((modS, xK_a       ), spawn "dmenu_run")
+        , ((modm, xK_s       ), gotoMenu)
+        , ((modS, xK_s       ), bringMenu)
+        , ((modm, xK_h       ), sendMessage Shrink)
+        , ((modS, xK_h       ), sendMessage ShrinkSlave)
+        , ((modm, xK_j       ), windows W.focusDown)
+        , ((modS, xK_j       ), windows W.swapDown)
+        , ((modm, xK_k       ), windows W.focusUp)
+        , ((modS, xK_k       ), windows W.swapUp)
+        , ((modm, xK_l       ), sendMessage Expand)
+        , ((modS, xK_l       ), sendMessage ExpandSlave)
+        , ((modm, xK_Return  ), windows W.swapMaster)
+        , ((modS, xK_Return  ), windows W.shiftMaster)
+
+        , ((modm, xK_n       ), refresh)
+        , ((modm, xK_m       ), windows W.focusMaster)
+        , ((modm, xK_comma   ), sendMessage (IncMasterN 1))
+        , ((modm, xK_period  ), sendMessage (IncMasterN (-1)))
+
+        , ((modm, xK_z       ), sendMessage (Toggle NBFULL))
+        , ((modm, xK_x       ), sendMessage ToggleStruts)
+        , ((modS, xK_c       ), kill)
+        , ((modm, xK_space   ), sendMessage NextLayout)
+        , ((modS, xK_space   ), setLayout $ XMonad.layoutHook conf)
+        ]
+        ++
+        [((modm .|. m, k), windows $ f i)
+                | (i, k) <- zip (workspaces conf) [xK_1..]
+                , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        ++
+        [((modC, k), windows $ swapWithCurrent i)
+                | (i, k) <- zip (workspaces conf) [xK_1..]]
+        ++
+        [((modm .|. m, k), screenWorkspace sc >>= flip whenJust (windows . f))
+                | (k, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+                , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+
+-- Mouse bindings
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+        [ ((modm, button1), (\w -> focus w >> windows W.shiftMaster))
+        , ((modm, button2), (\w -> focus w))
+        , ((modm, button3), (\w -> focus w))
+        , ((modm, button4), (\_ -> prevWS))
+        , ((modm, button5), (\_ -> nextWS))
+        ]
