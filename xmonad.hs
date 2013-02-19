2cca7216d96c1e2626c3724d9d0026ac3dc1f317
Ray Lehtiniemi <rayl@mail.com>
2013-02-19 12:53:20 -0700

    Set border width and default cursor
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,8 @@
 import XMonad
+import XMonad.Util.Cursor
 
 main = do
     xmonad $ defaultConfig
+        { borderWidth        = 2
+        , startupHook        = setDefaultCursor xC_left_ptr
+        }
