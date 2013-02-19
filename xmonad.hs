70fbfc36ea1670e2d11436c9783f249795f749d3
Ray Lehtiniemi <rayl@mail.com>
2013-02-19 13:15:05 -0700

    Import my current layouts
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,12 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.MouseResizableTile
+import XMonad.Layout.MultiColumns
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Layout.Named (named)
+import XMonad.Layout.NoBorders
 import XMonad.Util.Cursor
 import XMonad.Util.Run(spawnPipe)
 import System.IO
@@ -13,7 +19,7 @@ main = do
         { borderWidth        = 2
         , workspaces         = myWorkspaces
         , startupHook        = setDefaultCursor xC_left_ptr
-        , layoutHook         = avoidStruts $ layoutHook defaultConfig
+        , layoutHook         = myLayoutHook
         , manageHook         = manageHook defaultConfig <+> manageDocks
         , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
         , logHook            = dynamicLogWithPP defaultPP
@@ -37,4 +43,18 @@ myWorkspaces = zipWith (\n l -> show n ++ "-" ++ l)
                [1..9]
                ["goog","todo","book","hask","*","*","*","*","mp3s"]
 
+
+------------------------------------------------------------------------
+-- LAYOUTS
+------------------------------------------------------------------------
+myLayoutHook = myTransforms myLayouts
+
+myLayouts = named "3COL" (multiCol [1,1] 8 0.01 0.33)
+        ||| named "2COL" (multiCol [1,2] 8 0.01 0.50)
+        ||| named "FULL" (Full)
+        ||| named "DRAG" (mouseResizableTile { draggerType = BordersDragger })
+
+myTransforms = smartBorders
+             . avoidStruts
+             . mkToggle (single NBFULL)
 
